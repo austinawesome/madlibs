6076938d365cf5beedd2d7c564ee2ee47816a075
austin cummings and Sarah Lindmar <austin@>
2017-08-14 10:47:11 -0700

    use a for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,33 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
-    var person1Input = $("input#person1").val();
-    var person2Input = $("input#person2").val();
-    var animalInput= $("input#animal").val();
-    var exclamationInput = $("input#exclamation").val();
-    var verbInput = $("input#verb").val();
-    var nounInput = $("input#noun").val();
+        var formOne = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
 
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
+        formOne.forEach(function(blank) {
+          var userInput = $("input#" + blank).val();
+          $("." + formOne).text(userInput);
+        });
 
-    $("#story").show();
+        $("#story").show();
 
-    event.preventDefault();
-  });
-});
+        event.preventDefault();
+      });
+    });
+    // var person1Input = $("input#person1").val();
+    // var person2Input = $("input#person2").val();
+    // var animalInput= $("input#animal").val();
+    // var exclamationInput = $("input#exclamation").val();
+    // var verbInput = $("input#verb").val();
+    // var nounInput = $("input#noun").val();
+    //
+    // $(".person1").text(person1Input);
+    // $(".person2").text(person2Input);
+    // $(".animal").text(animalInput);
+    // $(".exclamation").text(exclamationInput);
+    // $(".verb").text(verbInput);
+    // $(".noun").text(nounInput);
+    //
+    // $("#story").show();
+    //
+//     // event.preventDefault();
+//   });
+// });
